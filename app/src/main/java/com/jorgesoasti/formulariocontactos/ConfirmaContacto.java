5e32a28301b5f3cdfdccd8639c09e33be0c71d60
jorge.soasti <jorgesoasti@gmail.com>
2017-09-28 11:19:39 -0500

    Implementación de startActivityForResult

--- app/src/main/java/com/jorgesoasti/formulariocontactos/ConfirmaContacto.java
+++ app/src/main/java/com/jorgesoasti/formulariocontactos/ConfirmaContacto.java
@@ -9,27 +9,37 @@ import android.widget.TextView;
 
 public class ConfirmaContacto extends AppCompatActivity {
 
+    //Declaración de Variables
+    TextView tvNombreCompleto;
+    TextView tvFechaNacimiento;
+    TextView tvTelefono;
+    TextView tvEmail;
+    TextView tvDesc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirma_contacto);
 
+        //Bundle para recibir los datos de la primera Actividad
         Bundle parametros = getIntent().getExtras();
 
-        final TextView tvNombreCompleto = findViewById(R.id.tvNombreCompleto);
-        TextView tvFechaNacimiento = findViewById(R.id.tvFechaNacimiento);
-        final TextView tvTelefono = findViewById(R.id.tvTelefono);
-        final TextView tvEmail = findViewById(R.id.tvEmail);
-        final TextView tvDesc = findViewById(R.id.tvDescContacto);
+        tvNombreCompleto = findViewById(R.id.tvNombreCompleto);
+        tvFechaNacimiento = findViewById(R.id.tvFechaNacimiento);
+        tvTelefono = findViewById(R.id.tvTelefono);
+        tvEmail = findViewById(R.id.tvEmail);
+        tvDesc = findViewById(R.id.tvDescContacto);
 
+        //Obtener datos enviados por la primera Actividad
         int anio = parametros.getInt("Anio");
         int mes = parametros.getInt("Mes");
         int dia = parametros.getInt("Dia");
         String nombre = parametros.getString("Nombre");
         String telefono = parametros.getString("Telefono");
         String email = parametros.getString("Email");
-        final String desc = parametros.getString("DescripContacto");
+        String desc = parametros.getString("DescripContacto");
 
+        //Colocar datos obtenidos en los TextView
         tvFechaNacimiento.setText("Fecha de Nacimiento: " + dia + "/" + mes + "/" + anio);
         tvNombreCompleto.setText(nombre);
         tvTelefono.setText(telefono);
@@ -37,21 +47,28 @@ public class ConfirmaContacto extends AppCompatActivity {
         tvDesc.setText(desc);
 
         Button btnEditar = findViewById(R.id.btnBotonEditar);
+
+        //Evento del Boton para editar los datos
         btnEditar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ConfirmaContacto.this, MainActivity.class);
+
+                //Obtener valores de los TextView
                 String nombre = tvNombreCompleto.getText().toString();
                 String telefono = tvTelefono.getText().toString();
                 String email = tvEmail.getText().toString();
                 String descrip = tvDesc.getText().toString();
 
+                //Colocar datos a enviar de vuelta a la primera Actividad
                 intent.putExtra("NombreC", nombre);
                 intent.putExtra("Telef", telefono);
                 intent.putExtra("Mail", email);
                 intent.putExtra("Des", descrip);
 
-                startActivity(intent);
+                setResult(RESULT_OK, intent);
+
+                onBackPressed();
             }
         });
     }
--- app/src/main/java/com/jorgesoasti/formulariocontactos/MainActivity.java
+++ app/src/main/java/com/jorgesoasti/formulariocontactos/MainActivity.java
@@ -1,48 +1,56 @@
 package com.jorgesoasti.formulariocontactos;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaración de Variables
+    private int REQUEST_CODE = 1;
+    DatePicker dpFechaNacimiento;
+    EditText edtNombreCompleto;
+    EditText edtTelefono;
+    EditText edtEmail;
+    EditText edtDescrip;
+    Button btnSiguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Bundle parametros = getIntent().getExtras();
-
-        Button btnSiguiente = findViewById(R.id.btnBotonSiguiente);
+        btnSiguiente = findViewById(R.id.btnBotonSiguiente);
 
         btnSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                //Creacion del Intent
                 Intent intent = new Intent(MainActivity.this, ConfirmaContacto.class);
 
-                final DatePicker dpFechaNacimiento = findViewById(R.id.dpFechaNacimiento);
-                final EditText edtNombreCompleto = findViewById(R.id.edtNombreCompleto);
-                final EditText edtTelefono = findViewById(R.id.edtTelefono);
-                final EditText edtEmail = findViewById(R.id.edtEmail);
-                final EditText edtDescrip = findViewById(R.id.edtDescripcionContacto);
+                dpFechaNacimiento = findViewById(R.id.dpFechaNacimiento);
+                edtNombreCompleto = findViewById(R.id.edtNombreCompleto);
+                edtTelefono = findViewById(R.id.edtTelefono);
+                edtEmail = findViewById(R.id.edtEmail);
+                edtDescrip = findViewById(R.id.edtDescripcionContacto);
 
+                //Obtener el valor de los EditText
                 String nombre = edtNombreCompleto.getText().toString();
                 String telefono = edtTelefono.getText().toString();
                 String email = edtEmail.getText().toString();
                 String descrip = edtDescrip.getText().toString();
 
+                //Obtener los valores del DatePicker
                 int anio = dpFechaNacimiento.getYear();
                 int mes = dpFechaNacimiento.getMonth() + 1;
                 int dia = dpFechaNacimiento.getDayOfMonth();
 
+                //Preparar los valores para el paso de informacion
                 intent.putExtra("Anio", anio);
                 intent.putExtra("Mes", mes);
                 intent.putExtra("Dia", dia);
@@ -50,10 +58,28 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("Telefono", telefono);
                 intent.putExtra("Email", email);
                 intent.putExtra("DescripContacto", descrip);
-                startActivity(intent);
+
+                //Iniciar la otra Actividad en espera de Resultados
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
+    }
 
+    //Método para recibir el resultado de la otra Actividad
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            //Obtener datos recibidos
+            String nombreC = data.getStringExtra("NombreC");
+            String telefono = data.getStringExtra("Telef");
+            String email = data.getStringExtra("Mail");
+            String descrip = data.getStringExtra("Des");
 
+            //Colocar los datos obtenidos en los EditText
+            edtNombreCompleto.setText(nombreC);
+            edtTelefono.setText(telefono);
+            edtEmail.setText(email);
+            edtDescrip.setText(descrip);
+        }
     }
 }
